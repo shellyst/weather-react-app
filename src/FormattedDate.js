7dd4f8a00f0996b31c397c69917865393cbc327f
shellyst <michellechristine.s91@gmail.com>
2021-06-20 18:41:11 -0400

    Update formatting of minutes and hours.

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -30,7 +30,13 @@ export default function FormattedDate(props) {
   ];
   let month = months[props.date.getMonth()];
   let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return (
     <div>
       {day} {month} {today} - {hours}:{minutes}
